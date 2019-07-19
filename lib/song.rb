f9ce2aa192cd5671bc54513ee009922bf26b641e
dynamic-router-3131 <jtboss47@gmail.com>
2019-07-19 17:41:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,6 +11,28 @@ class Song
   def save
     self.class.all << self
   end
+  
+  def self.create
+    var = self.new
+    var.save
+    var
+  end
+  
+  def self.new_by_name(name)
+    song = self.create
+    song.name = name 
+    song
+  end
+  
+  def self.create_by_name(name)
+    song = self.new_by_name(name)
+    song
+  end
+  
+  def self.find_by_name(name)
+
+        
+  end 
 
   def self.create 
     song = self.new 
@@ -70,4 +92,8 @@ class Song
 
 end
 
+<<<<<<< HEAD
 # binding.pry
+=======
+
+>>>>>>> 1cc73f6e01af965cb25d32eba77bda9a03212eda
